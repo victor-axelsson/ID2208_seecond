9ffbaba32bd6843a02deac03c9e17d77af932b98
Victor Axelsson <victor_axelsson@hotmail.com>
2017-02-05 16:55:47 +0100

    Created a better structure for the applications

--- create_client.sh
+++ create_client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wsimport -s src/main/java/client  http://localhost:9091/authentication?wsdl
+wsimport -s client/src/main/java/  -d client/client_description  http://localhost:9091/authentication?wsdl
--- create_wsdl.sh
+++ create_wsdl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wsgen -verbose -cp ./target/classes/ -d service_description  se.kth.webservice.second.service.Authentication -wsdl
+wsgen -verbose -cp ./server/target/classes/ -d service_description  se.kth.webservice.second.service.Authentication -wsdl
