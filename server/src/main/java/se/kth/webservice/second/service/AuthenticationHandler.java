acaefcb5cd27eb39cae30bb36ec75da900f6ed3b
RubbyRedson <gurnik2009@gmail.com>
2017-02-06 17:28:00 +0100

    Token as header on response

--- server/src/main/java/se/kth/webservice/second/service/AuthenticationHandler.java
+++ server/src/main/java/se/kth/webservice/second/service/AuthenticationHandler.java
@@ -1,6 +1,9 @@
 package se.kth.webservice.second.service;
 
+import org.w3c.dom.NodeList;
+
 import javax.xml.namespace.QName;
+import javax.xml.soap.*;
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
@@ -15,8 +18,22 @@ public class AuthenticationHandler implements SOAPHandler<SOAPMessageContext> {
     }
 
     public boolean handleMessage(SOAPMessageContext context) {
+        try {
+            SOAPBody body = context.getMessage().getSOAPBody();
+            NodeList elementsByTagName = body.getElementsByTagName("return");
+            if (elementsByTagName.getLength() > 0) {
+                String token = elementsByTagName.item(0).getTextContent();
+                SOAPHeader header = context.getMessage().getSOAPHeader();
+                QName headerName = new QName("http://service.second.webservice.kth.se/","token");
+                SOAPHeaderElement headerElement = header.addHeaderElement(headerName);
+                QName name = new QName("http://service.second.webservice.kth.se/","token");
+                SOAPElement symbol= headerElement.addChildElement(name);
+                symbol.addTextNode(token);
+            }
 
-
+        } catch (SOAPException e) {
+            e.printStackTrace();
+        }
 
 
         return true;
