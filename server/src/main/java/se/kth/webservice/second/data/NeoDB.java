db9cf8249525a381000c2dc1342be613a542c9a6
Victor Axelsson <victor_axelsson@hotmail.com>
2017-02-06 18:51:13 +0100

    Chnged to dynamic ids

--- server/src/main/java/se/kth/webservice/second/data/NeoDB.java
+++ server/src/main/java/se/kth/webservice/second/data/NeoDB.java
@@ -80,23 +80,19 @@ public class NeoDB {
         return start;
     }
 
-    public List<TravelPath> getRoutes(){
+    public List<TravelPath> getRoutes(String fromAirportId, String toAirportId){
+
+        //From = 12, to = 55;
 
         StatementResult result = session.run("MATCH path = \n" +
-                "(a1:Airport {id:'12'})-[:OUTGOING_FLIGHT]->(:Route)-[:INCOMMING_FLIGHT]->\n" +
+                "(a1:Airport {id: {fromId}})-[:OUTGOING_FLIGHT]->(:Route)-[:INCOMMING_FLIGHT]->\n" +
                 "(:Airport)-[:OUTGOING_FLIGHT]->(:Route)-[:INCOMMING_FLIGHT]->\n" +
                 "(:Airport)-[:OUTGOING_FLIGHT]->(:Route)-[:INCOMMING_FLIGHT]->\n" +
                 "(:Airport)-[:OUTGOING_FLIGHT]->(:Route)-[:INCOMMING_FLIGHT]->\n" +
                 "(:Airport)-[:OUTGOING_FLIGHT]->(:Route)-[:INCOMMING_FLIGHT]->\n" +
                 "(:Airport)-[:OUTGOING_FLIGHT]->(:Route)-[:INCOMMING_FLIGHT]->\n" +
-                "(a3:Airport {id: '55'}) return path");
-
-
-        /*
-        StatementResult result = session.run(
-                "MATCH (a:Person) WHERE a.name = {name} RETURN a.name AS name, a.title AS title",
-                parameters( "name", "Arthur" ));
-        */
+                "(a3:Airport {id: {toId}}) return path",
+                parameters("fromId", fromAirportId, "toId", toAirportId));
 
 
         List<TravelPath> availableTravelPaths = new ArrayList<TravelPath>();
